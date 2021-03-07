64580ae65e2dca6df6192da4348f3d12b5803767
Pathou80 <79732928+Pathou80@users.noreply.github.com>
2021-03-06 22:14:20 -0500

    web-calculator

---
+++ index.js
@@ -0,0 +1,11 @@
+function addnumbers () {
+let a = document.getelementbyid("box1").value
+const b = document.getElementById("box2").value
+a = 1
+b = 2
+var c = number(a) + number(b)
+
+
+
+document.getElementById("box3").value = c
+}
