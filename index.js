30537a4002faa25ee0b6f8e85ca7f9fd9ebc3721
Pathou80 <79732928+Pathou80@users.noreply.github.com>
2021-03-14 21:42:21 -0400

    Web-calculator part two

---
+++ index.js
@@ -0,0 +1,27 @@
+function addition() {
+    var num1 = Number(document.getElementById("num1").value);
+    var num2 = Number(document.getElementById("num2").value);
+    let result= num1 + num2
+    document.getElementById("result").innerHTML = result;
+}
+
+function subtraction() {
+    var num1 = Number(document.getElementById("num1").value);
+    var num2 = Number(document.getElementById("num2").value);
+    let result= num1 - num2
+    document.getElementById("result").innerHTML = result;
+}
+
+function multiplication() {
+    var num1 = Number(document.getElementById("num1").value);
+    var num2 = Number(document.getElementById("num2").value);
+    let result= num1 * num2
+    document.getElementById("result").innerHTML = result
+}
+
+function division() {
+    var num1 = Number(document.getElementById("num1").value);
+    var num2 = Number(document.getElementById("num2").value);
+    let result= num1 / num2
+    document.getElementById("result").innerHTML = result;
+}
